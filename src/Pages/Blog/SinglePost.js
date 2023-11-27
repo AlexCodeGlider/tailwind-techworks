e5b4e6ca8148817d65852e98e2a2dc124cc66732
Alejandro Leda <alejandro.leda@gmail.com>
2023-11-27 15:05:30 -0800

    Add new images and content for blog post

--- src/Pages/Blog/SinglePost.js
+++ src/Pages/Blog/SinglePost.js
@@ -6,9 +6,9 @@ import Blog1 from "../../assets/images/blog/blog-1.jpg";
 import Blog2 from "../../assets/images/blog/blog-2.jpg";
 import Blog3 from "../../assets/images/blog/blog-3.jpg";
 import BlogPost1 from "../../assets/images/blog/netflix.jpeg";
-import BlogPost2 from "../../assets/images/blog/blog-post-2.jpg";
+import BlogPost2 from "../../assets/images/blog/network_actors.png";
 import BlogPost3 from "../../assets/images/blog/blog-post-3.jpg";
-import BlogPost4 from "../../assets/images/blog/blog-post-4.jpg";
+import BlogPost4 from "../../assets/images/blog/degree_centrality.png";
 import BlogPost5 from "../../assets/images/blog/blog-post-5.jpg";
 import BlogPost6 from "../../assets/images/blog/blog-post-6.jpg";
 import Footer from "../../Layouts/CommonLayouts/Footer2";
@@ -16,7 +16,7 @@ import Navbar from "../../Layouts/CommonLayouts/Navbar3";
 
 const SinglePost = () => {
   //meta title
-  document.title = "TailWind TechWorks | Blog Post";
+  document.title = "TailWind TechWorks | Netflix Network";
   return (
     <React.Fragment>
       <Navbar navClass="sticky" />
@@ -28,7 +28,7 @@ const SinglePost = () => {
                 entertainment
               </div>
               <h1 className="h2 mb-n10" data-aos="fade-up">
-                Who are the key people in Netflix's success?
+                Who are the key players for Netflix?
               </h1>
             </Col>
           </Row>
@@ -41,29 +41,54 @@ const SinglePost = () => {
           <Row className="justify-content-center gh-1 mb-100">
             <Col lg={8} className="post-content">
               <p className="lead fw-medium">
-                Don't. Together you're waters face fowl every heaven whales
-                moving god him the third place let firmament fish waters set
-                there air dominion. Open earth so was face male life very i him
-                from gathered beginning.
+                In 2007, a decade after going into the film and television distribution 
+                business as a DVD rental by mail company, Netflix made a bold move that 
+                would change the way people watch television for the times to come. They 
+                launched the first commercially successful subscription video-on-demand service, 
+                forever changing the entertainment distribution and consumption paradigm. 
+                For the first time, the audience could now watch what they wanted, when they 
+                wanted, so long as the content they were looking for was available on the service. 
               </p>
               <p>
-                Together firmament good replenish they're moving fruitful over
-                living i. Life given you'll grass gathered seas fifth multiply
-                us also us brought blessed midst. Together you man lights he
-                form. Seasons saw fourth green rule fifth fish fruitful fowl
-                void was, spirit hath. They're days fruit multiply so creepeth
-                without whose. For be night. That firmament divided.
+                Today Netflix has over 214 million subscribers worldwide, including 74 million 
+                in the United States. As a data-driven company, they are known to perform extensive 
+                analysis on the viewership data they collect from their users. These analyses 
+                inform much of the decision-making processes within the company, including what 
+                content to license, commission, or produce. Questions such as what content to 
+                greenlight, who is a better actor for a given role, and who would be a good director 
+                for a given film are all answered with data within the organization. However, as of 
+                today, this information is mainly kept close to their chest. Identifying the key 
+                players in the film industry, especially those contributing to Netflix's success, 
+                can provide valuable insights into the elements driving the platform's popularity 
+                and growth. By understanding the roles and impact of these individuals, Netflix can 
+                better strategize content acquisition, creation, and marketing to maintain its 
+                competitive edge and satisfy its diverse subscriber base.
               </p>
               <p>
-                Can't midst all appear great Set greater land. Thing under.
-                Moveth is, void fowl them beast. Which fowl fruitful. Us every
-                green seed likeness. Air the set after you'll of. Gathering that
-                he beast be fish it air air male made, two.
+                In the following analysis, I aim to answer the overarching question of who the key 
+                individuals for Netflix’s success are by performing network analyses. This project 
+                will focus on the titles currently available for viewing in the United States; however, 
+                these same concepts could be extended to other countries. Unfortunately, Netflix doesn’t 
+                make their viewership information available, but beginning about a year and a half ago, 
+                they started posting their daily top 10 ranking in film and television. We will use this 
+                data as a proxy measure of success by awarding each title 10 points for each day it ranks 
+                number one, 9 points for each day it ranks number two, and so on. The final score is the 
+                sum of points during the entire period for which a given title ranked in the top 10. Any 
+                title released since they began posting rankings publicly but that did not rank in the top 
+                10 is awarded the default value of 1 point. This data is then enriched by cross-referencing 
+                the titles with existing movie databases, such as IMDb, to obtain the key talent involved. 
+                With this data at hand, we build a weighted undirected graph where the nodes represent the 
+                talent, the edges represent the movies they worked together in, and the edge weights are the 
+                total ranking score for a given title. Network analysis was chosen to answer the research 
+                question due to its ability to uncover complex relationships and interactions among individuals 
+                involved in the film industry. By examining the connections between key personnel in various 
+                projects, we can gain insights into the structure and dynamics of the industry and identify 
+                the most influential players based on their positions within the network. 
               </p>
             </Col>
           </Row>
-          <Row className="gh-1 gv-1 mb-100">
-            <Col sm={6} className="col-12" data-aos="fade-up">
+          <Row className="gh-1 gv-1 mb-100 justify-content-center">
+            <Col sm={8} className="col-12" data-aos="fade-up">
               <Link
                 to={BlogPost2}
                 className="gallery-item gallery-item-lg"
@@ -73,52 +98,31 @@ const SinglePost = () => {
                 <img src={BlogPost2} alt="" />
               </Link>
             </Col>
-            <div className="col-12 col-sm-6" data-aos="fade-up">
-              <Link
-                to={BlogPost3}
-                className="gallery-item gallery-item-lg"
-                data-fancybox="gallery-1"
-                data-aos="fade"
-              >
-                <img src={BlogPost3} alt="" />
-              </Link>
-            </div>
           </Row>
-          <Row className="justify-content-center gh-1 mb-100">
+          <Row className="justify-content-center gh-1 mb-10">
             <Col lg={8} className="post-content">
-              <h3>Moving void, let fill in midst open.</h3>
+              <h3>How you meaure importance matters.</h3>
               <p>
-                Male under yielding whose his appear sea green. Greater doesn't
-                land had earth won't Them fowl bring day without there isn't
-                whales god that replenish. Seas greater brought stars you his
-                day likeness waters heaven gathering. Fowl she'd given open
-                green morning after divide bring won't his multiply them whose
-                firmament after stars make sixth he for fourth stars thing fly
-                fruitful sixth signs air it they're made saying god so fowl
-                thing.
+                Any network with connections between individuals, where the 
+                connections capture the relationship between them is a social 
+                network. Analyzing these networks can give us great insight 
+                about the people in the network like who are real influencers,
+                who are most connected, etc. To the end goal of finding the most 
+                important nodes (individuals) in the network, we will focus on two metrics knows as...
               </p>
-              <h4>Own whose male beginning thing?</h4>
-              <p>
-                Male under yielding whose his appear sea green. Greater doesn't
-                land had earth won't Them fowl bring day without there isn't
-                whales god that replenish. Seas greater brought stars you his
-                day likeness waters heaven gathering. Fowl she'd given open
-                green morning after divide bring won't his multiply them whose
-                firmament after stars make sixth he for fourth stars thing fly
-                fruitful sixth signs air it they're made saying god so fowl
-                thing.
-              </p>
+              <h4>Centrality Measures</h4>
             </Col>
           </Row>
           <Row className="gh-1 gv-1 mb-100">
             <Col sm={6} md={4} className="col-12" data-aos="fade-up">
+              <h3>Degree Centrality</h3>
               <Link
                 to={BlogPost4}
                 className="gallery-item gallery-item-lg"
                 data-fancybox="gallery-2"
                 data-aos="fade"
               >
-                <img src={BlogPost4} alt="" />
+                <img src={BlogPost4} alt="Degree Centrality"/>
               </Link>
             </Col>
             <Col sm={6} md={4} className="col-12" data-aos="fade-up">
@@ -142,6 +146,24 @@ const SinglePost = () => {
               </Link>
             </Col>
           </Row>
+          <Row className="justify-content-center gh-1 mb-100">
+            <Col lg={8} className="post-content">
+                <p>
+                  One of the most surprising results from these analyses is that, 
+                  although most of the people involved in the network are actors, 
+                  most of the players central to Netflix are not. Degree centrality, 
+                  which is based on the fact that important people have many connections, 
+                  predictably shows that producers primarily dominate the top rank. This 
+                  understanding coincides with a producer's role in producing film and 
+                  television content, where they are tasked with making the connections 
+                  necessary for a project to be realized. On the other hand, eigenvector 
+                  centrality, which aims to rate players by the importance of their neighbors, 
+                  shows a higher percentage of actors. Finally, PageRank centrality, which 
+                  rates players by the centrality of their connections, offers the most 
+                  diverse group of individuals.
+                </p>
+              </Col>
+            </Row>
           <Row className="justify-content-center gh-1 mb-n4">
             <Col lg={6} className="col-12 text-center">
               <svg
